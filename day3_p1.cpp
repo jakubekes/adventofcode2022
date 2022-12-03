6f947f5e5f425aa8daa754af45bb609bef2a17f1
Jakub <jakubs1993@o2.pl>
2022-12-03 19:40:01 +0100

    Day3

---
+++ day3_p1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+int main() {
+
+	std::ifstream  plik;
+	plik.open("day3_input.txt", std::ios::in);
+	std::string temp;
+	unsigned long int answer{};
+
+	if (plik.is_open()) {
+		while(getline(plik,temp)){
+			bool found{};
+			std::cout<<temp<<" ";
+			size_t l = temp.length()/2;
+			for (size_t i{}; i<l; i++){
+				for (size_t j{l}; j<temp.length(); j++){
+					if (temp[i]==temp[j]){
+						if(temp[i]>=97){
+							answer+=temp[i]-96;
+							std::cout<<temp[i]<<" "<<answer<<" (>) \n";
+						} else {
+							answer+=temp[i]-38;
+							std::cout<<temp[i]<<" "<<answer<<" (<) \n";
+						}
+						found = true;
+						break;
+					}
+				}
+				if(found)break;						
+			}
+		
+		}
+		std::cout<<"Answer: "<<answer<<"\n";
+		
+		
+	}
+	else {
+		
+		std::cout << "Nie otwarto pliku";
+		
+	}
+
+	plik.close();
+
+
+}
