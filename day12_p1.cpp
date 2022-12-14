4c6b6c7f7a30217f4367d6a70c7f0942de0335ee
Jakub <25148319+jakubekes@users.noreply.github.com>
2022-12-14 22:48:50 +0100

    Day12_start

--- day12_p1.cpp
+++ day12_p1.cpp
@@ -4,15 +4,17 @@
 #include <vector>
 #include <stack>
 #include <chrono>
+#include <algorithm>
 
 struct point{
 	
-	point(int x, int y):x(x),y(y){}
+	point(int y, int x, int val):y(y),x(x),val(val){}
 	
-	int x;
 	int y;
-	int val{};
-	std::stack <char> connections;
+	int x;
+	char val{};
+	std::stack <point*> connections;
+	bool used{};
 };
 
 int main() {
@@ -23,13 +25,13 @@ int main() {
 	plik.open("day12_input.txt", std::ios::in);
 	std::string temp;
 	unsigned int answer{};
-	std::stack <point> mainStack;
+	std::stack <point*> mainStack;
 	
 	if (plik.is_open()) {
 		std::cout << "File opened\n";
 
-		std::vector<std::vector<char>> v;
-		std::vector<char> vtemp;
+		std::vector<std::vector<point*>> v;
+		std::vector<point*> vtemp;
 		int sx{}, sy{}, ex{}, ey{};
 		int j{};
 		
@@ -39,12 +41,14 @@ int main() {
 				if(temp[i]=='S'){
 					sx=i;
 					sy=j;
-				}
+				}	
 				if(temp[i]=='E'){
 					ex=i;
 					ey=j;
-				}				
-				vtemp.push_back(temp[i]);	
+					vtemp.push_back(new point(j,i,'z'));
+					continue;
+				}							
+				vtemp.push_back(new point(j,i,temp[i]));	
 							
 			}
 			
@@ -54,19 +58,86 @@ int main() {
 		}		
 		plik.close();
 		
-		mainStack.push(point(sx,sy));
-		if(sx-1>0 && v[sy][sx-1]<2)mainStack.top().connections.push('L');
-		//if(sx+1<v[0].size() && v[sy][sx+1]<2)mainStack.top().connections.push('R');
-		//if(sy-1>0 && v[sy-1][sx]<2)mainStack.top().connections.push('U');
-		//if(sy+1<v.size() && v[sy][sx-1]<2)mainStack.top().connections.push('D');
-		for(int i{}; i<temp.length(); i++){
-		for(int i{}; i<temp.length(); i++){
+		mainStack.push(v[sy][sx]);
+		v[sy][sx]->used=true;
 		
-		}
-		}
+		if(sx-1>=0 && v[sy][sx-1]->val-97<2) mainStack.top()->connections.push(v[sy][sx-1]);
+		if(sx+1<v[0].size() && v[sy][sx+1]->val-97<2) mainStack.top()->connections.push(v[sy][sx+1]);
+		if(sy-1>=0 && v[sy-1][sx]->val-97<2) mainStack.top()->connections.push(v[sy-1][sx]);
+		if(sy+1<v.size() && v[sy+1][sx]->val-97<2) mainStack.top()->connections.push(v[sy+1][sx]);
 		
+		long int steps{};
+		std::vector<long int> stepsv;
+		bool returned{};
+		while(!mainStack.empty()){
+			//std::cout<<mainStack.top()->val<<" ";
+			//if (mainStack.top()->x==ex && mainStack.top()->y==ey){
+			if (mainStack.top()->x==ex && mainStack.top()->y==ey){
+				//std::cout<<"IF1***: "<<mainStack.top()->y<<" "<<mainStack.top()->x<<" steps:"<<steps<<"\n";
+				stepsv.push_back(steps);
+				steps--;
+				mainStack.top()->used=false;
+				mainStack.pop();
+				returned=true;
+				//std::cout<<"IF1***: "<<mainStack.top()->y<<" "<<mainStack.top()->x<<" steps:"<<steps<<"\n";
+			} else if(!mainStack.top()->connections.empty()){
+				//std::cout<<"IF2: "<<mainStack.top()->y<<" "<<mainStack.top()->x<<"\n";
+				point* p = mainStack.top();
+				mainStack.push(p->connections.top());
+				p->connections.top()->used=true;
+				p->connections.pop();
+				steps++;
+				returned = false;
+			} else if (returned==false) {
+				//std::cout<<"IF3: "<<mainStack.top()->y<<" "<<mainStack.top()->x<<"\n";
 
-		std::cout << "Answer: " << answer << "\n";
+				sx = mainStack.top()->x;
+				sy = mainStack.top()->y;
+				//std::cout<<"Poz x: "<<sx<<"\n";
+				if(sx-1>=0 && v[sy][sx-1]->used==false && v[sy][sx-1]->val-v[sy][sx]->val<2) {
+				//std::cout<<"IF3 1\n";
+					mainStack.top()->connections.push(v[sy][sx-1]);
+				}
+				if(sx+1<v[0].size() && v[sy][sx+1]->used==false && v[sy][sx+1]->val-v[sy][sx]->val<2) {
+				//std::cout<<"IF3 2\n";
+					mainStack.top()->connections.push(v[sy][sx+1]);
+				}
+				if(sy-1>=0 && v[sy-1][sx]->used==false && v[sy-1][sx]->val-v[sy][sx]->val<2) {
+				//std::cout<<"IF3 3\n";
+					mainStack.top()->connections.push(v[sy-1][sx]);
+				}
+				
+				if(sy+1<v.size() && v[sy+1][sx]->used==false && v[sy+1][sx]->val-v[sy][sx]->val<2) {
+				//std::cout<<"IF3 4\n";
+					mainStack.top()->connections.push(v[sy+1][sx]);
+				}
+						
+				/*if(mainStack.top()->y==3 && mainStack.top()->x==7){
+				std::cout<<"TEST VAL::"<<sy<<" "<<sx<<" "<<v[sy-1][sx]->val<<"\n";	
+				}*/
+						
+				if(mainStack.top()->connections.empty()){ //slepy zaulek
+					//std::cout<<"IF3: slepyzaulek sy:"<<sy<<" sx:"<<sx<<" val:"<<v[sy][sx]->val<<"\n";	
+					steps--;
+					mainStack.top()->used=false;
+					mainStack.pop();
+					returned=true;
+				}
+				
+			} else if (returned==true) {
+				//std::cout<<"IF4: "<<mainStack.top()->y<<" "<<mainStack.top()->x<<"\n";	
+				steps--;
+				mainStack.top()->used=false;
+				mainStack.pop();
+				returned=true;
+			} 
+			
+			
+		}
+		
+		std::sort(stepsv.begin(),stepsv.end());
+
+		std::cout << "Answer: " << stepsv[0] << "\n";
 
 		auto finish = std::chrono::high_resolution_clock::now();
 		std::chrono::duration<double> elapsed = finish - start;
